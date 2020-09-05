2a511fda16709dc202874843a68896d7266883d2
Joel Pick <joel.l.pick@gmail.com>
2020-09-05 22:18:40 +0100

    extract multiple items per image

--- R/measuRe.R
+++ R/measuRe.R
@@ -13,10 +13,12 @@
 #' @export
 #' @examples
 #' #measuRe()
-measuRe <- function(image_folder, output_file, x11=FALSE){
-	#	image_folder="~/Dropbox/measuRe/test"
+measuRe <- function(image_folder, x11=FALSE){
+	#	image_folder="~/Dropbox/measuRe_package/test"
 	#	output_file="~/Dropbox/measuRe/lengths.Rdata"
-
+	
+	output_file <- paste0(image_folder,"/SavedData.Rdata")
+	
 	if(file.exists(output_file)){ 
 		load(output_file) 
 	}else{
@@ -24,6 +26,7 @@ measuRe <- function(image_folder, output_file, x11=FALSE){
 	}
 
 	all_image_names <- list.files(image_folder)
+	all_image_names <- all_image_names[!all_image_names %in% "SavedData.Rdata"]
 	image_names <- all_image_names[!all_image_names %in% names(all_data)]
 	done_images <- length(all_image_names[all_image_names %in% names(all_data)])
 
@@ -42,6 +45,8 @@ measuRe <- function(image_folder, output_file, x11=FALSE){
 	#i=1
 		n <- paste0((done_images+i),"/",length(all_image_names))
 
+		j <- 1
+
 		if(add_removeQ!="f"){ ## if not finished
 			image <- magick::image_read(paste0(image_folder,"/",image_names[i]))
 			
@@ -51,6 +56,12 @@ measuRe <- function(image_folder, output_file, x11=FALSE){
 				while(add_removeQ=="a"){
 						select_points <- graphics::locator(1)
 					
+					##add
+					if( select_points$x<image_dat$add_max_x & select_points$y<max(image_dat$box_y) & select_points$x>image_dat$add_min_x & select_points$y>min(image_dat$box_y)) {
+						##save data
+						#all_data[[image_names[i]]] <- group_data
+						j=j+1
+					}
 					##next
 					if( select_points$x<image_dat$next_max_x & select_points$y<max(image_dat$box_y) & select_points$x>image_dat$next_min_x & select_points$y>min(image_dat$box_y)) {
 						add_removeQ <- "b"
@@ -61,16 +72,29 @@ measuRe <- function(image_folder, output_file, x11=FALSE){
 					else if(select_points$x<image_dat$zoom_max_x & select_points$y<max(image_dat$box_y) & select_points$x>image_dat$zoom_min_x & select_points$y>min(image_dat$box_y)){
 						##deleted data and replot
 						graphics::polygon(image_dat$background_x,image_dat$background_y, col="white", border=FALSE)
-						text(mean(image_dat$background_x),mean(image_dat$box_y),"Click on top left hand corner and bottom right hand corner of where you want to zoom in on")
+						text(mean(image_dat$background_x),mean(image_dat$box_y),"Click on top left hand corner and bottom right hand corner\nof where you want to zoom in on")
 						corners <- locator(2)
 						image_dat <- basic_plot(image,xlim=sort(corners$x),ylim=sort(corners$y),n=n)
-						lines(group_data$x,group_data$y, type="o", col="red", pch=19, cex=1)
+						#lines(group_data$x,group_data$y, type="o", col="red", pch=19, cex=1)
+						for(k in unique(group_data$item)) lines(y~x,group_data[group_data$item==k,],type="o", col="red", pch=19, cex=1)
 					}					
-					##redo
-					else if(select_points$x<image_dat$redo_max_x & select_points$y<max(image_dat$box_y) & select_points$x>image_dat$redo_min_x & select_points$y>min(image_dat$box_y)){
+					##zoom out
+					else if(select_points$x<image_dat$zoomOut_max_x & select_points$y<max(image_dat$box_y) & select_points$x>image_dat$zoomOut_min_x & select_points$y>min(image_dat$box_y)){
+						##deleted data and replot
+						image_dat <- basic_plot(image,n=n)
+						for(k in unique(group_data$item)) lines(y~x,group_data[group_data$item==k,],type="o", col="red", pch=19, cex=1)
+					}##redo all
+					else if(select_points$x<image_dat$redoAll_max_x & select_points$y<max(image_dat$box_y) & select_points$x>image_dat$redoAll_min_x & select_points$y>min(image_dat$box_y)){
 						##deleted data and replot
 						group_data <- data.frame()
 						image_dat <- basic_plot(image,n=n)
+					}
+					##redo one item
+					else if(select_points$x<image_dat$redo_max_x & select_points$y<max(image_dat$box_y) & select_points$x>image_dat$redo_min_x & select_points$y>min(image_dat$box_y)){
+						##deleted data and replot
+						group_data <- subset(group_data, item<j)
+						image_dat <- basic_plot(image,n=n)
+						for(k in unique(group_data$item)) lines(y~x,group_data[group_data$item==k,],type="o", col="red", pch=19, cex=1)
 					}
 					##finish
 					else if(select_points$x<image_dat$finish_max_x & select_points$y<max(image_dat$box_y) & select_points$x>image_dat$finish_min_x & select_points$y>min(image_dat$box_y)){
@@ -84,8 +108,8 @@ measuRe <- function(image_folder, output_file, x11=FALSE){
 					}
 					else{ 
 						points(select_points$x,select_points$y, col="red", pch=20, cex=1)
-						if(nrow(group_data)>0) lines(c(select_points$x,group_data$x[nrow(group_data)]),c(select_points$y,group_data$y[nrow(group_data)]), col="red", lwd=2)	
-						group_data <- rbind(group_data, data.frame(file=image_names[i], x=select_points$x, y=select_points$y) )
+						if(nrow(group_data[group_data$item==j,])>0) lines(c(select_points$x,group_data$x[nrow(group_data)]),c(select_points$y,group_data$y[nrow(group_data)]), col="red", lwd=2)	
+						group_data <- rbind(group_data, data.frame(file=image_names[i], item=j, x=select_points$x, y=select_points$y) )
 		
 					}
 				}
--- R/plot_functions.R
+++ R/plot_functions.R
@@ -1,3 +1,13 @@
+#' image_details Function
+#'
+#' Plots image
+#' @param image path to image
+#' @param xlim subset of image to plot. Default plots all x
+#' @param ylim subset of image to plot. Default plots all y
+image_details <- function(image, xlim=NULL, ylim=NULL){
+
+}
+
 
 #' plot_image Function
 #'
@@ -10,50 +20,77 @@
 plot_image <- function(image, xlim=NULL, ylim=NULL){
 	image_details <- c(width = magick::image_info(image)["width"][[1]], height = magick::image_info(image)["height"][[1]])
 		
-		if(is.null(xlim)) xlim <- c(0,image_details[1])
-		if(is.null(ylim)) ylim <- c(0,image_details[2])
+	if(is.null(xlim)) xlim <- c(0,image_details[1])
+	if(is.null(ylim)) ylim <- c(0,image_details[2])
 
-		length_y <- diff(ylim)
-		min_y <- ylim[1]
-		min_y_new <- min_y + length_y * - 0.15
-		ylim[1] <- min_y_new
-		box_y <- c(min_y+ length_y * - 0.05,min_y_new,min_y_new, min_y+ length_y * - 0.05)
-		
-		background_y <- c(min_y+ length_y * - 0.2,min_y,min_y, min_y+ length_y * - 2)
-		background_x <- rep(xlim+xlim*c(-0.1,0.1),each=2)
-
-
-		length_x <- diff(xlim) / 15
-		next_min_x <- xlim[1] + length_x
-		next_max_x <- xlim[1] + length_x*3
-		zoom_min_x <- xlim[1] + length_x*4
-		zoom_max_x <- xlim[1] + length_x*6
-		redo_min_x <- xlim[1] + length_x*7
-		redo_max_x <- xlim[1] + length_x*9
-		finish_min_x <- xlim[1] + length_x*10
-		finish_max_x <- xlim[1] + length_x*12
-		progress <- xlim[1] + length_x*14
-
-		op <- graphics::par(mar=c(0,0,0,0), mfrow=c(1,1))
-		#on.exit(graphics::par(op))
-
-		plot(NA, xlim=xlim, ylim=ylim)
-		plot(image,add=TRUE)
+	length_y <- diff(ylim)
+	min_y <- ylim[1]
+	min_y_new <- min_y + length_y * - 0.16
+	ylim[1] <- min_y_new
+	box_y <- c(min_y+ length_y * - 0.04,min_y_new,min_y_new, min_y+ length_y * - 0.04)
+	
+	background_y <- c(min_y+ length_y * - 0.2,min_y,min_y, min_y+ length_y * - 2)
+	background_x <- rep(xlim+xlim*c(-0.1,0.1),each=2)
 
 
-		return(list(box_y=box_y,
-			length_x=length_x,
-			next_min_x=next_min_x,
-			next_max_x=next_max_x,
-			zoom_min_x=zoom_min_x,
-			zoom_max_x=zoom_max_x,
-			redo_min_x=redo_min_x,
-			redo_max_x=redo_max_x,
-			finish_min_x=finish_min_x,
-			finish_max_x=finish_max_x,
-			background_y=background_y,
-			background_x=background_x,
-			progress=progress))
+	length_x <- diff(xlim) / 16
+	add_min_x <- xlim[1] + length_x*0.5
+	add_max_x <- xlim[1] + length_x*2.1
+	next_min_x <- xlim[1] + length_x*2.4
+	next_max_x <- xlim[1] + length_x*4.1
+	zoom_min_x <- xlim[1] + length_x*4.4
+	zoom_max_x <- xlim[1] + length_x*6.1
+	zoomOut_min_x <- xlim[1] + length_x*6.4
+	zoomOut_max_x <- xlim[1] + length_x*8.1
+	redo_min_x <- xlim[1] + length_x*8.4
+	redo_max_x <- xlim[1] + length_x*10.1
+	redoAll_min_x <- xlim[1] + length_x*10.4
+	redoAll_max_x <- xlim[1] + length_x*12.1
+	finish_min_x <- xlim[1] + length_x*12.4
+	finish_max_x <- xlim[1] + length_x*14.1
+	progress <- xlim[1] + length_x*15
+
+	op <- graphics::par(mar=c(0,0,0,0), mfrow=c(1,1))
+	#on.exit(graphics::par(op))
+
+	plot(NA, xlim=xlim, ylim=ylim)
+	plot(image,add=TRUE)
+
+
+	return(list(box_y=box_y,
+		length_x=length_x,
+		add_min_x=add_min_x,
+		add_max_x=add_max_x,
+		next_min_x=next_min_x,
+		next_max_x=next_max_x,
+		zoom_min_x=zoom_min_x,
+		zoom_max_x=zoom_max_x,
+		zoomOut_min_x=zoomOut_min_x,
+		zoomOut_max_x=zoomOut_max_x,
+		redo_min_x=redo_min_x,
+		redo_max_x=redo_max_x,
+		redoAll_min_x=redoAll_min_x,
+		redoAll_max_x=redoAll_max_x,
+		finish_min_x=finish_min_x,
+		finish_max_x=finish_max_x,
+		background_y=background_y,
+		background_x=background_x,
+		progress=progress))
+}
+
+#' button_plot Function
+#'
+#' Plots buttons on image
+#' @param min_x minimum x coord
+#' @param max_x max x coord
+#' @param y y cords of box
+#' @param text text in box
+#' @param back_col background colour
+#' @param text_col text colour
+button_plot <- function(min_x,max_x,y, text,back_col,  text_col){
+	x <- c(min_x,min_x,max_x,max_x)
+	graphics::polygon(x,y, col=back_col, border=TRUE,xpd=TRUE)
+	text(mean(x), mean(y),text, col = text_col, font = 2, cex = 1.2)
 }
 
 #' plot_buttons Function
@@ -62,23 +99,19 @@ plot_image <- function(image, xlim=NULL, ylim=NULL){
 #' @param id image details
 #' @param n number of image
 plot_buttons <- function(id,n){
-		next_x <- c(id$next_min_x,id$next_min_x,id$next_max_x,id$next_max_x)
-		zoom_x <- c(id$zoom_min_x,id$zoom_min_x,id$zoom_max_x,id$zoom_max_x)
-		redo_x <- c(id$redo_min_x,id$redo_min_x,id$redo_max_x,id$redo_max_x)
-		finish_x <- c(id$finish_min_x,id$finish_min_x,id$finish_max_x,id$finish_max_x)
-		
-		graphics::polygon(id$background_x,id$background_y, col="white", border=FALSE)
+	# id <- image_dat 
+	graphics::polygon(id$background_x,id$background_y, col="white", border=FALSE)
 
-		graphics::polygon(next_x,id$box_y, col="black", border=TRUE,xpd=TRUE)
-		graphics::polygon(zoom_x,id$box_y, col="coral2", border=TRUE,xpd=TRUE)
-		graphics::polygon(redo_x,id$box_y, col="cornflowerblue", border=TRUE,xpd=TRUE)
-		graphics::polygon(finish_x,id$box_y, col="red4", border=TRUE,xpd=TRUE)
+	button_plot(id$add_min_x,id$add_max_x,id$box_y,"Add", text_col = "white", back_col="black")
+	button_plot(id$next_min_x,id$next_max_x,id$box_y,"Next", text_col = "white", back_col="black")
+	button_plot(id$zoom_min_x,id$zoom_max_x,id$box_y,"Zoom", text_col = "white", back_col="coral2")
+	button_plot(id$zoomOut_min_x,id$zoomOut_max_x,id$box_y,"Zoom\nOut", text_col = "white", back_col="coral2")
+	button_plot(id$redo_min_x,id$redo_max_x,id$box_y,"Redo", text_col = "white", back_col="cornflowerblue")
+	button_plot(id$redoAll_min_x,id$redoAll_max_x,id$box_y,"Redo\nAll", text_col = "white", back_col="cornflowerblue")
 
-		text(mean(c(id$next_min_x,id$next_max_x)), mean(id$box_y),"Next", col = "white", font = 2, cex = 1.2)
-		text(mean(c(id$zoom_min_x,id$zoom_max_x)), mean(id$box_y),"Zoom", col = "white", font = 2, cex = 1.2)
-		text(mean(c(id$redo_min_x,id$redo_max_x)), mean(id$box_y),"Redo", col = "white", font = 2, cex = 1.2)
-		text(mean(c(id$finish_min_x,id$finish_max_x)), mean(id$box_y),"Finish", col = "white", font = 2, cex = 1.2)
-		text(id$progress, mean(id$box_y),n, col = "black", font = 2, cex = 1.2)
+	button_plot(id$finish_min_x,id$finish_max_x,id$box_y,"Finish", text_col = "white", back_col="red4")
+
+	text(id$progress, mean(id$box_y),n, col = "black", font = 2, cex = 1.2)
 
 		
 }
